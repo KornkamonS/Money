05d15c038e083b49eb4b818c018f7401d0e8883b
KornkamonS <tunasang_18@hotmail.com>
2017-03-22 23:06:52 +0700

    Change interface (activity -> fragment)

---
+++ app/src/main/java/tarikalovebird/money/Income_fragment.java
@@ -0,0 +1,27 @@
+package tarikalovebird.money;
+
+/**
+ * Created by TunasanG on 22/3/2560.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by TunasanG on 22/3/2560.
+ */
+
+public class Income_fragment extends Fragment{
+
+    View myview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myview=inflater.inflate(R.layout.content_income,container,false);
+        return myview;
+
+    }
+}
--- app/src/main/java/tarikalovebird/money/MainActivity.java
+++ app/src/main/java/tarikalovebird/money/MainActivity.java
@@ -1,15 +1,8 @@
 package tarikalovebird.money;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,23 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    public static final String KEY_DRAWABLE_ID = "drawableId";
-
-    private String[] mDrawerTitle = {"Cover", "Guitar", "Bass", "Drum"};
-
-    private ImageButton targetBut;
-    private TextView t;
-    private Target target;
-    private TextView day;
-
+        FragmentManager defaultFragment = getSupportFragmentManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,28 +21,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        targetBut = (ImageButton) findViewById(R.id.imageTargetBut);
-        t=(TextView) findViewById(R.id.nameTarget);
-        day=(TextView) findViewById(R.id.CoundownDay) ;
-        target=new Target(this);
-        t.setText(target.getTargetName());
-        day.setText(target.getCountDown());
-
-        targetBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), AddTarget.class);
-                startActivityForResult(i, 1);
-            }
-        }) ;
-
-        targetBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), AddTarget.class);
-                startActivity(i);
-            }
-        }) ;
+        defaultFragment.beginTransaction().replace(R.id.content_frame,new Home_fragment()).commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -73,47 +32,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == 1) {
-
-            if(resultCode == RESULT_OK){
-                //Update List
-                t.setText(target.getTargetName());
-                day.setText(target.getCountDown());
-                int idpic=0;
-                 switch ( target.getTargetType())
-                {
-                    case R.id.TypeFood: targetBut.setBackgroundResource(R.drawable.type_food);
-                        break;
-                    case R.id.TypeGift:targetBut.setBackgroundResource(R.drawable.type_gift);
-                        break;
-                    case R.id.TypeLearning:targetBut.setBackgroundResource(R.drawable.type_learning);
-                        break;
-                    case R.id.TypeMusic:targetBut.setBackgroundResource(R.drawable.type_music);
-                        break;
-                    case R.id.TypeTechno: targetBut.setBackgroundResource(R.drawable.type_techno);
-                        break;
-                    case R.id.AddType: targetBut.setBackgroundResource(R.drawable.type_add);
-                        break;
-                    default: targetBut.setBackgroundResource(R.drawable.ic_menu_gallery);
-                }
-
-            }
-            if (resultCode == RESULT_CANCELED) {
-                //Do nothing?
-            }
-        }
-    }//onActivityResult
-
-    /*@Override
-    public void onResume()
-    {  // After a pause OR at startup
-        super.onResume();
-        //Refresh your stuff here
-        t.setText(target.getTargetName());
-        day.setText(target.getCountDown());
-    }*/
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -152,23 +71,24 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_Home) {
+        FragmentManager a= getSupportFragmentManager();
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+            a.beginTransaction().replace(R.id.content_frame,new Home_fragment()).commit();
 
-        } else if (id == R.id.nav_Income) {
-            /*Intent i = new Intent(getApplicationContext(), IncomeActivity.class);
-            startActivity(i);*/
+        } else if (id == R.id.nav_income) {
+            
+            a.beginTransaction().replace(R.id.content_frame,new Income_fragment()).commit();
+        } else if (id == R.id.nav_outcome) {
 
-        } else if (id == R.id.nav_Outcom) {
+            a.beginTransaction().replace(R.id.content_frame,new Outcome_fragment()).commit();
+        } else if (id == R.id.nav_summary) {
 
-        } else if (id == R.id.nav_Summary) {
-
+            a.beginTransaction().replace(R.id.content_frame,new Summary_fragment()).commit();
         } else if (id == R.id.nav_share) {
 
+
         } else if (id == R.id.nav_send) {
-
-        } else if (id == R.id.nav_TIPs) {
-
-        } else if (id == R.id.nav_Noti) {
 
         }
 
